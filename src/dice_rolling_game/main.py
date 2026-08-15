d7fcae0b87119713529b0a2ebb34bcac43516fde
Firas <97522852+fifolio@users.noreply.github.com>
2026-08-15 13:25:25 +0000

    Complete Dice Rolling Game

--- src/dice_rolling_game/main.py
+++ src/dice_rolling_game/main.py
@@ -1,2 +1,13 @@
-def main() -> None:
-    print("Hello from dice-rolling-game!")
+import random
+
+while True:
+    choice = input("Roll the dice? (y/n): ").lower()
+    if choice == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'{die1}, {die2}')
+    elif choice == 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print("Print invalid choice")
